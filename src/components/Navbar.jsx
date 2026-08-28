4a7437d98aa95421dadf2e2c8146bc6cc510c691
Michael Oliveira <micholiveira.md@gmail.com>
2026-08-27 23:14:23 -0300

    ajustes finais

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-const NavBar = () => {
+const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 z-50 md:p-9 p-3">
       <img src="/images/nav-logo.svg" alt="nav-logo" className="md:w-24 w-20" />
@@ -6,4 +6,4 @@ const NavBar = () => {
   );
 };
 
-export default NavBar;
+export default Navbar;
